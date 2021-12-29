618bc07f5dde0338b6804334c5ef2af8346268f2
brucestull <bruce.stull@gmail.com>
2021-12-29 06:57:05 -0500

    Update valid_parentheses.py
    Added comments.

--- CodeWars/valid_parentheses.py
+++ CodeWars/valid_parentheses.py
@@ -24,30 +24,49 @@ def valid_parentheses(input_string):
     # Evaluate each character.
     for character in input_string:
         
-        # ')'
+        # If character is ')':
         if character == ')':
+            # If there is already one or more opening parentheses:
             if number_open >= 1:
+                # Reduce number of unclosed parentheses by one.
                 number_open -= 1
+                # Sequence is currently valid.
                 is_valid = True
+                # Go to beginning of loop and check next character.
                 continue
+            # If there are no opening parentheses before this closing one:
             else:
+                # Sequence is invalid.
                 is_valid = False
+                # Break out of loop.
                 break
-        # '('
+        
+        # Check if character is '(':
         elif character == '(':
+            # Add one to the number of open parentheses.
             number_open += 1
+            # Sequence is valid.
             is_valid = True
+            # Continue to next character.
             continue
+        # Otherwise, character is not either parentheses.
         else:
+            # Sequence is valid. Letters and numbers are okay.
             is_valid = True
+            # Continue to next character.
             continue
-
+    
+    # If there are any open parentheses or sequence has already determined to be invalid:
     if number_open != 0 or not is_valid:
+        # Set flag as False.
         is_valid = False
     
+    # If all open parentheses have been closed.
     elif number_open == 0:
+        # Sequence is valid.
         is_valid = True
     
+    # Return the validity status.
     return is_valid
 
 test = ''
